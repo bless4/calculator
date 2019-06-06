0116932c28a89de8acbf765de14ccade131ce3b7
bless4 <kwangmart12@gmail.com>
2019-06-06 10:47:23 +0200

    input added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 
 import React, {Component} from 'react';
 import './App.css';
-import Button from './components/Button';
+import {Button} from './components/Button';
+import {Input} from './components/Input';
 
 
 class App extends Component {
+  constructor(props){
+    super(props);
+   
+    this.state = {
+      input: ""
+  };
+  }
+
   render(){
     return (
     <div className="app">
       <div className="calc-wrapper">
+        <Input input={this.state.input}/>
         <div className="row">
       <Button>7</Button>
       <Button>8</Button>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,14 +5,14 @@ import './Button.css';
 const isOperator =val => {
     return !isNaN(val) || val==="." || val=== "=";
 }
-const Button = props =>  <div 
+ export const Button = props =>  (
+ <div 
 className={`button-wrapper ${
     isOperator(props.children) ? null: "operator"
    
 }` }>  {props.children}
 </div>
 
-   
+ );   
         
 
-export default Button;
