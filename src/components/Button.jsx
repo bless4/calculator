634329b3ab4223c95c725f46f7af0856271bc335
bless4 <kwangmart12@gmail.com>
2019-06-06 12:04:52 +0200

    finish react-calculator

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,18 +1,17 @@
-import React from 'react';
-import './Button.css';
+import React from "react";
+import "./Button.css";
 
+const isOperator = val => {
+  return !isNaN(val) || val === "." || val === "=";
+};
 
-const isOperator =val => {
-    return !isNaN(val) || val==="." || val=== "=";
-}
- export const Button = props =>  (
- <div 
-className={`button-wrapper ${
-    isOperator(props.children) ? null: "operator"
-   
-}` }>  {props.children}
-</div>
-
- );   
-        
-
+export const Button = props => (
+  <div
+    className={`button-wrapper ${
+      isOperator(props.children) ? null : "operator"
+    }`}
+    onClick={() => props.handleClick(props.children)}
+  >
+    {props.children}
+  </div>
+);
