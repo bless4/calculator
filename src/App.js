8d241f728eaa0baeeb5701f332e60fdbbae688af
Martin Sone <kwangmart12@gmail.com>
2020-06-01 13:34:41 +0200

    codebase for buttons reduce using map and arrow functions

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
       input: ""
     };
   }
+ 
 
   addToInput = val => {
     this.setState({ input: this.state.input + val });
@@ -22,34 +23,36 @@ class App extends Component {
     this.setState({ input: math.eval(this.state.input) });
   };
 
+
+ 
   render() {
+    //To shortten the code base with arr...to be continue
+    let arr = [7,8, 9,'/']
+    let arr1 =[4,5,6,'*']
+    let arr2 =[1,2,3,'+']
+    let arr3 = ['.',0,'-']
+
+
+
     return (
       <div className="app">
         <div className="calc-wrapper">
           <Input input={this.state.input} />
           <div className="row">
-            <Button handleClick={this.addToInput}>7</Button>
-            <Button handleClick={this.addToInput}>8</Button>
-            <Button handleClick={this.addToInput}>9</Button>
-            <Button handleClick={this.addToInput}>/</Button>
+       
+    {arr.map(arr =><Button handleClick={this.addToInput}>{arr}</Button>)} 
+          
           </div>
           <div className="row">
-            <Button handleClick={this.addToInput}>4</Button>
-            <Button handleClick={this.addToInput}>5</Button>
-            <Button handleClick={this.addToInput}>6</Button>
-            <Button handleClick={this.addToInput}>*</Button>
+          {arr1.map(arr1 =><Button handleClick={this.addToInput}>{arr1}</Button>)} 
           </div>
           <div className="row">
-            <Button handleClick={this.addToInput}>1</Button>
-            <Button handleClick={this.addToInput}>2</Button>
-            <Button handleClick={this.addToInput}>3</Button>
-            <Button handleClick={this.addToInput}>+</Button>
+          {arr2.map(arr2 =><Button handleClick={this.addToInput}>{arr2}</Button>)} 
           </div>
           <div className="row">
-            <Button handleClick={this.addToInput}>.</Button>
-            <Button handleClick={this.addToInput}>0</Button>
+          {arr3.map(arr3 =><Button handleClick={this.addToInput}>{arr3}</Button>)} 
             <Button handleClick={() => this.handleEqual()}>=</Button>
-            <Button handleClick={this.addToInput}>-</Button>
+          
           </div>
           <div className="row">
             <ClearButton handleClear={() => this.setState({ input: "" })}>
